4d509f56dc5609515219fb5b945c72248f66ca08
sektionschef <the.stevee@gmail.com>
2023-05-07 21:08:03 +0200

    added perlin.js

--- project/public/art.js
+++ project/public/art.js
@@ -10,6 +10,26 @@
 // Do your thing! 🚀
 
 SVG.on(document, 'DOMContentLoaded', function () {
+    // for (var i = 0; i < 30000; i++) {
     var draw = SVG().addTo('#badAssCanvas')
     draw.rect(100, 100).move(100, 50).fill('#f06')
+
+    var polyline = draw.polyline('0,0 100,50 50,100').fill('none').stroke({ width: 1, color: '#818181' })
+    // }
 })
+
+
+
+noise.seed(Math.random());
+
+for (var x = 0; x < 80; x++) {
+    for (var y = 0; y < 80; y++) {
+        // All noise functions return values in the range of -1 to 1.
+
+        // noise.simplex2 and noise.perlin2 for 2d noise
+        var value = noise.simplex2(x / 100, y / 100);
+        // console.log(value);
+
+
+    }
+}

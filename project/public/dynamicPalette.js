3f99f202c6bc75d439b6ef035ac008df4340f7aa
sektionschef <the.stevee@gmail.com>
2023-05-13 20:58:43 +0200

    almost done palette

--- project/public/dynamicPalette.js
+++ project/public/dynamicPalette.js
@@ -3,30 +3,40 @@ class dynamicPalette {
     constructor(drawing, baseColor) {
         this.drawing = drawing;
         this.baseColor = tinycolor(baseColor);
-        let _color = tinycolor(baseColor);
+        let _color_beginning = tinycolor(baseColor);
+        let _color_end = tinycolor(baseColor);
         console.log(this.baseColor);
 
+        this.length = 9;
         this.upAndDown = 4;
 
         // this.palette = [];
-        this.palette = Array(9);
+        this.palette = Array(this.length);
 
         // 4 down
-        for (var i = this.upAndDown - 1; i >= 0; i--) {
-            // _color.darken(2 * i);
-            _color.desaturate(2 * i)
-            this.palette.push(_color.toHexString())
-        }
+        // for (var i = this.upAndDown - 1; i >= 0; i--) {
+        // _color.darken(2 * i);
+        // _color.desaturate(2 * i)
+        // this.palette.push(_color.toHexString())
+        // }
 
         // base color in the middle
-        this.palette.push(this.baseColor.toHexString());
+        this.palette[this.upAndDown] = this.baseColor.toHexString();
 
-        for (var i = 0; i < this.upAndDown; i++) {
-            // this.palette.push(this.baseColor.lighten(3 * i).toHexString());
-            // this.palette.push(this.baseColor.saturate(3 * i).toHexString());
+
+        for (var i = 1; i < (this.upAndDown + 1); i++) {
+            _color_beginning.darken(4 * i);
+            // // _color_beginning.desaturate(4 * i);
+            // console.log(this.upAndDown - 1 - i);
+            this.palette[this.upAndDown - i] = _color_beginning.toHexString();
         }
 
-        // console.log(this.baseColor);
+        for (var i = 1; i < (this.upAndDown + 1); i++) {
+            _color_end.lighten(4 * i);
+            // _color_end.saturate(4 * i);
+            this.palette[(this.upAndDown + i)] = _color_end.toHexString();
+        }
+
         console.log(this.palette);
     }
 

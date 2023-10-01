4417af5dffcf9ac91f54ba099a1b4a5c82365247
sektionschef <the.stevee@gmail.com>
2023-10-01 19:50:32 +0200

    update new fxlens

--- project/src/index.js
+++ project/src/index.js
@@ -1,13 +1,16 @@
-// console.log(fxhash)
-// console.log(fxrand())
-
+// demonstrate seed reset
+// for (let i = 0; i < 10; i++) {
+//   console.log(i, $fx.rand(), $fx.randminter())
+//   $fx.rand.reset();
+//   $fx.randminter.reset();
+// }
 
 console.log('');
 console.info(`fxhash: %c${fxhash}`, 'font-weight: bold');
 console.log('');
 
-const sp = new URLSearchParams(window.location.search);
-// console.log(sp);
+const sp = new URLSearchParams(window.location.search)
+console.log(sp);
 
 // this is how to define parameters
 $fx.params([
@@ -28,13 +31,15 @@ $fx.params([
   //   options: {
   //     min: 1,
   //     max: 10,
-  //     step: 0.00000000000001,
+  //     step: 0.0001,
   //   },
   // },
+
   // {
   //   id: "bigint_id",
   //   name: "A bigint",
   //   type: "bigint",
+  //   update: "code-driven",
   //   //default: BigInt(Number.MAX_SAFE_INTEGER * 2),
   //   options: {
   //     min: Number.MIN_SAFE_INTEGER * 4,
@@ -43,37 +48,52 @@ $fx.params([
   //   },
   // },
   // {
+  //   id: "string_id_long",
+  //   name: "A string long",
+  //   type: "string",
+  //   update: "code-driven",
+  //   //default: "hello",
+  //   options: {
+  //     minLength: 1,
+  //     maxLength: 512,
+  //   },
+  // },
+  // {
   //   id: "select_id",
   //   name: "A selection",
   //   type: "select",
+  //   update: "code-driven",
   //   //default: "pear",
   //   options: {
   //     options: ["apple", "orange", "pear"],
-  //   }
+  //   },
   // },
   // {
   //   id: "color_id",
   //   name: "A color",
   //   type: "color",
+  //   update: "code-driven",
   //   //default: "ff0000",
   // },
   // {
   //   id: "boolean_id",
   //   name: "A boolean",
   //   type: "boolean",
+  //   update: "code-driven",
   //   //default: true,
   // },
   // {
   //   id: "string_id",
   //   name: "A string",
   //   type: "string",
+  //   update: "code-driven",
   //   //default: "hello",
   //   options: {
   //     minLength: 1,
-  //     maxLength: 64
-  //   }
+  //     maxLength: 512,
+  //   },
   // },
-]);
+])
 
 // this is how features can be defined
 $fx.features({
@@ -86,39 +106,86 @@ $fx.features({
 
 console.log($fx.getFeatures());
 
-// log the parameters, for debugging purposes, artists won't have to do that
-// console.log("Current param values:")
-// Raw deserialize param values 
-// console.log($fx.getRawParams())
-// Added addtional transformation to the parameter for easier usage
-// e.g. color.hex.rgba, color.obj.rgba.r, color.arr.rgb[0] 
 console.log($fx.getParams())
 
-// how to read a single raw parameter
-// console.log("Single raw value:")
-// console.log($fx.getRawParam("color_id"));
-// how to read a single transformed parameter
-// console.log("Single transformed value:")
-// console.log($fx.getParam("color_id"));
+function main() {
+  // // log the parameters, for debugging purposes, artists won't have to do that
+  // console.log("Current param values:");
+  // // Raw deserialize param values
+  // console.log($fx.getRawParams());
+  // // Added addtional transformation to the parameter for easier usage
+  // // e.g. color.hex.rgba, color.obj.rgba.r, color.arr.rgb[0]
+  // console.log($fx.getParams());
 
-// update the document based on the parameters
-// document.body.style.background = $fx.getParam("color_id").hex.rgba
-// document.body.innerHTML = `
-// <p>
-// url: ${window.location.href}
-// </p>
-// <p>
-// hash: ${$fx.hash}
-// </p>
-// <p>
-// params:
-// </p>
-// <pre>
-// ${$fx.stringifyParams($fx.getRawParams())}
-// </pre>
-// <pre style="color: white;">
-// ${$fx.stringifyParams($fx.getRawParams())}
-// </pre>
-// `
+  // // how to read a single raw parameter
+  // console.log("Single raw value:");
+  // console.log($fx.getRawParam("color_id"));
+  // // how to read a single transformed parameter
+  // console.log("Single transformed value:");
+  // console.log($fx.getParam("color_id"));
 
+  // const getContrastTextColor = (backgroundColor) =>
+  //   ((parseInt(backgroundColor, 16) >> 16) & 0xff) > 0xaa
+  //     ? "#000000"
+  //     : "#ffffff"
 
+  // const bgcolor = $fx.getParam("color_id").hex.rgba
+  // const textcolor = getContrastTextColor(bgcolor.replace("#", ""))
+
+  // update the document based on the parameters
+  // document.body.style.background = bgcolor
+  // document.body.innerHTML = `
+  // <div style="color: ${textcolor};">
+  //   <p>
+  //   hash: ${$fx.hash}
+  //   </p>
+  //   <p>
+  //   minter: ${$fx.minter}
+  //   </p>
+  //   <p>
+  //   iteration: ${$fx.iteration}
+  //   </p>
+  //   <p>
+  //   inputBytes: ${$fx.inputBytes}
+  //   </p>
+  //   <p>
+  //   context: ${$fx.context}
+  //   </p>
+  //   <p>
+  //   params:
+  //   </p>
+  //   <pre>
+  //   ${$fx.stringifyParams($fx.getRawParams())}
+  //   </pre>
+  // <div>
+  // `
+
+  // const btn = document.createElement("button")
+  // btn.textContent = "emit random params"
+  // btn.addEventListener("click", () => {
+  //   $fx.emit("params:update", {
+  //     number_id: $fx.getRandomParam("number_id"),
+  //     bigint_id: $fx.getRandomParam("bigint_id"),
+  //     string_id_long: $fx.getRandomParam("string_id_long"),
+  //     select_id: $fx.getRandomParam("select_id"),
+  //     color_id: $fx.getRandomParam("color_id"),
+  //     boolean_id: $fx.getRandomParam("boolean_id"),
+  //     string_id: $fx.getRandomParam("string_id"),
+  //   })
+  //   main()
+  // })
+  // document.body.appendChild(btn)
+}
+
+main()
+
+$fx.on(
+  "params:update",
+  (newRawValues) => {
+    // opt-out default behaviour
+    if (newRawValues.number_id === 5) return false
+    // opt-in default behaviour
+    return true
+  },
+  (optInDefault, newValues) => main()
+)
